6064bbe745ec855d8a4fd338f458a391c09c3d27
lishuai <lishuai@zuoyebang.com>
2019-12-09 15:05:43 +0800

    feat: 处理shell脚本

--- branch.sh
+++ branch.sh
@@ -5,16 +5,16 @@ echo "Shell 传递参数实例！"
 # echo "第一个参数为：$1";
 # echo "第二个参数为：$2";
 # echo "第三个参数为：$3";
-
 cd ./packages/$1
 pwd
 
-echo `git status --porcelain`
+echo $2
 
-# if [`$2` === 1]
-# then
-#   git add .
-#   git commit -m "`$3`"
-# fi
-
-# git checkout $4
+if [[ `git status --porcelain` ]]; then
+  # Changes
+  git add .
+  git commit -m $2
+else
+  # No changes
+  git checkout $3
+fi
